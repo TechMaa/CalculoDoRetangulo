5d0c3af09e8ac894e4965d7708226b43f117f97a
DeveloperMatheus97 <matheuscaitanosouza18@outlook.com>
2022-05-02 13:18:37 -0300

    first commit

---
+++ Ex02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, a, area, perimetro, diagonal;
+
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = b * a;
+            perimetro = 2 * (b + a);
+            diagonal = Math.Sqrt(Math.Pow(b, 2.0) + Math.Pow(a, 2.0));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+                Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+        }
+    }
+}
